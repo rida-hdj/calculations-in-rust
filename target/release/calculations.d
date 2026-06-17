22c04c63b152a31fe5ef07ce32d3e7765c562de9
rida-hdj <rida.hdj@users.noreply.github.com>
2026-06-17 16:17:04 +0100

    add target

---
+++ target/release/calculations.d
@@ -0,0 +1,1 @@
+/home/rida/dev/programming-langs/rust/calculations/target/release/calculations: /home/rida/dev/programming-langs/rust/calculations/src/main.rs
---
+++ target/release/deps/calculations-eb2eef46724b61a5.d
@@ -0,0 +1,5 @@
+/home/rida/dev/programming-langs/rust/calculations/target/release/deps/calculations-eb2eef46724b61a5.d: src/main.rs
+
+/home/rida/dev/programming-langs/rust/calculations/target/release/deps/calculations-eb2eef46724b61a5: src/main.rs
+
+src/main.rs:
